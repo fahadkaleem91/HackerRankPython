6d0c296285a6f931ba3419607e34d8636aabdec2
Fahad Kaleem <fkaleem@Reddwerkss-MacBook-Pro-5.local>
2016-01-20 15:39:07 -0600

    Initial commit

---
+++ Introduction/WhatsYourName.py
@@ -0,0 +1,4 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+firstname = raw_input()
+lastname = raw_input()
+print("Hello "+firstname+" "+lastname+"! You just delved into python.")
